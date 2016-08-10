cc2f62469e12313c3d91ac76f6dd9faf0086c6e3
root <zfq_work@163.com>
2016-08-10 13:58:48 +0800

    pipeline update

--- TagBlogs/pipelines.py
+++ TagBlogs/pipelines.py
@@ -4,7 +4,6 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-from cnblogs.spiders.url_spider import UrlSpider
 import json
 
 
