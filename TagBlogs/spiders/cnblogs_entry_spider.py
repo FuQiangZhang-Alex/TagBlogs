b4aab14316bfdffd24def7ec9b7a58692775dd5b
root <zfq_work@163.com>
2016-08-31 14:56:23 +0800

    complete entry spider

--- TagBlogs/spiders/cnblogs_entry_spider.py
+++ TagBlogs/spiders/cnblogs_entry_spider.py
@@ -13,27 +13,28 @@ class CNBlogsEntrySpider(sp.Spider):
     ]
     new_entries = {}
 
-    # def __init__(self):
-    #     ss = SitemapSpider()
-    #     entries = ss.extract()
-    #     self.new_entries = {}
-    #     for entry in entries:
-    #         url = entry['url']
-    #         cate = entry['cate']
-    #         pn = entry['page_number']
-    #         self.start_urls.append(url)
-    #         if cate == 'undefined':
-    #             for page_num in range(2, pn + 1):
-    #                 self.start_urls.append(url + 'default.html?page=' + str(page_num))
-    #         else:
-    #             for page_num in range(2, pn + 1):
-    #                 self.start_urls.append(url + '#p' + str(page_num))
-    #         f = open(file='a.txt', mode='w')
-    #         f.write(str(self.start_urls))
+    def __init__(self):
+        ss = SitemapSpider()
+        entries = ss.extract()
+        self.new_entries = {}
+        for entry in entries:
+            url = entry['url']
+            cate = entry['cate']
+            pn = entry['page_number']
+            self.start_urls.append(url)
+            if cate == 'undefined':
+                for page_num in range(2, pn + 1):
+                    self.start_urls.append(url + 'default.html?page=' + str(page_num))
+            else:
+                for page_num in range(2, pn + 1):
+                    self.start_urls.append(url + '#p' + str(page_num))
+            f = open(file='a.txt', mode='w')
+            f.write(str(self.start_urls))
     #         self.new_entries[url] = entry
 
     def parse(self, response):
         self.logger.info('parse')
+        entry_list = []
         selector = sp.Selector(response)
         entry = BlogEntry()
         entry_url = response.url
@@ -54,7 +55,22 @@ class CNBlogsEntrySpider(sp.Spider):
                                     , post_pubs))
             if len(post_urls) == len(post_headers) == len(post_summaries) == len(post_pubs) \
                     == len(post_homes) == len(post_authors):
-                print(len(post_authors), post_authors)
+                for i in range(0, len(post_urls)):
+                    post_header = post_headers[i]
+                    post_url = post_urls[i]
+                    post_summary = post_summaries[i]
+                    post_pub = post_pubs[i]
+                    post_home_search = regex.search(pattern='http://www\.cnblogs\.com/.+/(?=p)', string=post_url[0])
+                    post_home = post_home_search.captures()[0] if post_home_search else ''
+                    post_author = ''
+                    entry['url'] = post_url
+                    entry['header'] = post_header
+                    entry['summary'] = post_summary
+                    entry['author_url'] = post_home
+                    entry['author'] = post_author
+                    entry['pub_time'] = post_pub
+                    entry['cate'] = cate
+                    entry_list.append(entry)
 
         else:
             post_headers = selector.xpath('//div[@class="day"]/div[@class="postTitle"]/a/text()').extract()
@@ -77,31 +93,5 @@ class CNBlogsEntrySpider(sp.Spider):
                     entry['author'] = post_author
                     entry['pub_time'] = post_pub
                     entry['cate'] = cate
-
-    def parse_bk(self, response):
-        selector = sp.Selector(response)
-        post_list = selector.xpath('//div[@id="post_list"]/div[@class="post_item"]')
-        entry_url = response.url
-        key_search = regex.search(pattern='^http://www\.cnblogs\.com/.+/', string=entry_url)
-        cate = ''
-        if key_search:
-            key = key_search.captures()[0]
-            cate = self.new_entries[key]['cate']
-        else:
-            cate = 'undefined'
-        for post_item in post_list:
-            entry = BlogEntry()
-            post_url = post_item.xpath('div[@class="post_item_body"]/h3/a/@href').extract()
-            post_header = post_item.xpath('div[@class="post_item_body"]/h3/a/text()').extract()
-            post_summary = post_item.xpath('div[@class="post_item_body"]/p/text()').extract()
-            post_home = post_item.xpath('div[@class="post_item_body"]/div[@class="post_item_foot"]/a/@href').extract()
-            post_author = post_item.xpath('div[@class="post_item_body"]/div[@class="post_item_foot"]/a/text()').extract()
-            post_pub = post_item.xpath('div[@class="post_item_body"]/div[@class="post_item_foot"]/text()').extract()
-            entry['url'] = post_url[0] if post_url else ''
-            entry['header'] = post_header[0] if post_header else ''
-            entry['summary'] = post_summary[0] if post_summary else ''
-            entry['author_url'] = post_home[0] if post_home else ''
-            entry['author'] = post_author[0] if post_author else ''
-            entry['pub_time'] = post_pub[0] if post_pub else ''
-            entry['cate'] = cate
-            return entry
+                    entry_list.append(entry)
+        return entry_list
